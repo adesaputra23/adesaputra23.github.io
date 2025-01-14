aaf5c32cd696526c0a78496e80eb10b17cdeacd0
Ade Saputra <sarjanakertas11@gmail.com>
2025-01-14 16:08:14 +0700

    update template

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+    colors: {
+      'body-color': '#F5EFFF',
+      'blue': '#1fb6ff',
+      'purple': '#7e5bef',
+      'pink': '#ff49db',
+      'orange': '#ff7849',
+      'green': '#13ce66',
+      'yellow': '#ffc82c',
+      'gray-dark': '#273444',
+      'gray': '#8492a6',
+      'gray-light': '#d3dce6',
+      'blue-01': '#CDC1FF',
+    },
+  },
+  plugins: [],
+}
+
